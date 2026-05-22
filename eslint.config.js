95df85da5cacc7bb5bfbd2b933207787f7dbc65f
FranManfredi <85314753+FranManfredi@users.noreply.github.com>
2026-05-22 04:05:30 -0300

    feat: add eslint-plugin-cypress for improved Cypress linting support

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 const prettierConfig = require('eslint-config-prettier/flat');
+const cypress = require('eslint-plugin-cypress');
 
 module.exports = defineConfig([
   {
@@ -14,5 +15,28 @@ module.exports = defineConfig([
     ],
   },
   ...expoConfig,
+  {
+    files: ['cypress/**/*.js', 'cypress/**/*.ts'],
+    plugins: {
+      cypress,
+    },
+    languageOptions: {
+      globals: {
+        cy: 'readonly',
+        Cypress: 'readonly',
+        describe: 'readonly',
+        context: 'readonly',
+        it: 'readonly',
+        before: 'readonly',
+        beforeEach: 'readonly',
+        after: 'readonly',
+        afterEach: 'readonly',
+        expect: 'readonly',
+      },
+    },
+    rules: {
+      ...cypress.configs.recommended.rules,
+    },
+  },
   prettierConfig,
 ]);
